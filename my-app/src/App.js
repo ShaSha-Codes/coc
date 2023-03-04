e26a3a4edb8443ac8affc1dd28c37d71df080d93
Shaurya Sharma <79473274+ShaSha-Codes@users.noreply.github.com>
2023-03-04 11:21:04 +0530

    Firebase Config Added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Testing
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
