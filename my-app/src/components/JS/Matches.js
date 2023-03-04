769bc7b05eafce998d041c10f075edaf189c6195
Shaurya Sharma <79473274+ShaSha-Codes@users.noreply.github.com>
2023-03-05 03:13:18 +0530

    Added Stuff

--- my-app/src/components/JS/Matches.js
+++ my-app/src/components/JS/Matches.js
@@ -2,9 +2,12 @@ import React from "react";
 import profile from '../../assets/profile.jpg'
 import { useSelector } from 'react-redux'
 import { selectUserdata } from '../../feature/navSlice'
-import { doc, updateDoc,getDoc } from "firebase/firestore";
+import { doc, updateDoc,getDoc,addDoc,collection } from "firebase/firestore";
 import {db} from '../../firebase'
 import Stack from '@mui/material/Stack';
+import { nanoid } from 'nanoid'
+
+
 const Matches = () => {
   const userValue =  useSelector(selectUserdata)
   const [comp,setComp]=React.useState([])
@@ -18,6 +21,35 @@ const Matches = () => {
       return data
    
     
+  }
+
+  const connectUsers = async (email) => {
+
+        let newChannel=nanoid(5)
+        const ref = doc(db, "userInfo", email);
+        const docSnap = await getDoc(ref)
+        const data=docSnap.data()
+        await updateDoc(ref, {
+            accept: [...data.accept, userValue.email],
+            chat:newChannel,
+            timestemps:Date.now()
+          });
+
+          
+        const ref2 = doc(db, "userInfo", userValue.email);
+        const docSnap2 = await getDoc(ref2)
+        const data2=docSnap2.data()
+        await updateDoc(ref2, {
+          accept: [...data2.accept, email],
+          chat:newChannel,
+          timestemps:Date.now()
+          });
+
+        const collectionsRef = collection(db, newChannel);
+        await addDoc(collectionsRef, {
+          comment: []
+          })
+         
   }
 
   const swiped2=async()=>{
@@ -36,7 +68,7 @@ const Matches = () => {
             <div className="left">{tempData.name}</div>
             
             <div  className="right" style={{ marginLeft: "200px" }}>
-              <button className="accept">+</button>
+              <button className="accept" onClick={()=>connectUsers(tempData.email)}>+</button>
               <button className="reject">-</button>
             </div>
 
