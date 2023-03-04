d3ad88eb28e9f1035bc9faca476e2a375ac1879f
Shaurya Sharma <79473274+ShaSha-Codes@users.noreply.github.com>
2023-03-04 14:38:03 +0530

    Login Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Registration from './pages/Registration';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/registration" element={<Registration />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
